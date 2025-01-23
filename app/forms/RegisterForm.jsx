c59ad9717020dabbb30c3fe28b37b79f7e6471fc
omarbougarne <omarbougarne98@gmail.com>
2025-01-23 12:20:23 +0100

    Feat Register

--- app/forms/RegisterForm.jsx
+++ app/forms/RegisterForm.jsx
@@ -1,28 +1,59 @@
 import { useState } from "react";
-import { View, Text, Button, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
-
+import { View, Text, Button, StyleSheet, TextInput, TouchableOpacity, Alert } from 'react-native';
+import axios from "axios";
 
 function RegisterForm() {
-   
+
+    const [email, setEmail] = useState('')
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
+    
+   const handleRegister = async () =>{
+        try{
+            const response = axios.post('http://localhost:5000/api/auth/register',{
+                email,
+                name,
+                password
+            });
+            if(response === 200){
+                Alert.alert     ('Registered successfully')       
+            }else {
+                console.log('Error response status:', response.status);
+                console.log('Error response data:', response.data);
+                Alert.alert('Error in registration', response.data.message || 'An error occurred');
+            }
+   }catch(error){
+        console.log(error)
+        Alert.alert('Error in registration')
+   }
+}
+
 
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Welcome to the Movie app 🎥</Text>
             <Text style={styles.title}>Register</Text>
             <TextInput 
-                style = {styles.input}
-                placeholder = "Name"
+                style={styles.input}
+                placeholder="Name"
+                value={name}
+                onChangeText={setName}
             />
             <TextInput 
-                style = {styles.input}
-                placeholder = "Email"
+                style={styles.input}
+                placeholder="Email"
+                value={email}
+                onChangeText={setEmail}
             />
             <TextInput 
-                style = {styles.input}
-                placeholder = "Password"
+                style={styles.input}
+                placeholder="Password"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
             />
             
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity style={styles.button} onPress = {handleRegister}>
                 <Text title="Register" style= {styles.buttonText}>Register</Text>
             </TouchableOpacity>
         </View>
